e418ca399b447fc1409464fe92b307336aa8159c
Refael Siagian <juarisiagian21106@gmail.com>
2024-11-20 14:52:59 +0700

    migration

---
+++ database/migrations/2024_11_13_032734_create_presences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('presences', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->string('employee_id'); // Foreign key to employee table
+            $table->boolean('present'); // Presence status, true or false
+            $table->date('date'); // Date of presence record
+            $table->timestamps(); // Created_at and updated_at timestamps
+
+            // Foreign key constraint (assuming employee_id is a string)
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('presences');
+    }
+};
